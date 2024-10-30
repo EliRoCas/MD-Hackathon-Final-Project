d46bd343b860b69133b145caba8eabc92e23cf5f
Eli RoCas <elir_94@hotmail.com>
2024-10-29 19:46:31 -0500

    Solution without "i" character

---
+++ CHALLENGES.md
@@ -0,0 +1,15 @@
+RETO 1 
+Desencripta el mensaje de Ada Lovelace 
+
+^[()`~^*/?`[()^+`-~()#[$()/~()%\[]()}+[()[{=~¿()=$?%?!?]¡~()¿()`¬^~()[{=~()?+^[]=?()^\{()*~¡[$[{()¡[()?=[]`\¬]()¿()[{;+[$__~()`~]=\]+~
+ 
+
+• El mensaje está en español
+• No hay diferencia entre mayúsculas y minúsculas
+• Las únicas letras que no se encuentran en el mensaje son: g, k, w, x
+• Las vocales están representadas de la siguiente forma:
+a ?
+e [
+i \
+o ~
+u +
---
+++ decryptChallenge.js
@@ -0,0 +1,51 @@
+// Don't exist: g, k, w, x
+function findCharacter(encryptedMessage) {
+  const hints = {
+    "?": "a",
+    "[": "e",
+    "\\": "i",
+    "~": "o",
+    "+": "u",
+    "¬": "ó",
+    "()": " ",
+    "%": "b",
+    "`": "c",
+    "¡": "d",
+    ";": "f",
+    "!": "j", 
+    "-": "h",
+    "/": "l",
+    "^": "m",
+    "]": "n", 
+    "*": "p", 
+    "}": "q", 
+    $: "r",
+    "{": "s",
+    "=": "t",
+    "#": "v",
+    "¿": "y",
+   
+    _: "z",
+  };
+
+  encryptedMessage = encryptedMessage.replace(/\(\)/g, " ").replace(/\\/g, "i");
+
+  let partialDecryption = encryptedMessage
+    .split("")
+    .map((letter) => {
+      return hints[letter] || letter;
+    })
+    .join("");
+  return partialDecryption;
+}
+
+const encryptedMessage =
+  "^[()`~^*/?`[()^+`-~()#[$()/~()%[]()}+[()[{=~¿()=$?%?!?]¡~()¿()`¬^~()[{=~()?+^[]=?()^{()*~¡[$[{()¡[()?=[]`¬]()¿()[{;+[$__~()`~]=]+~";
+const decryptedMessage = findCharacter(encryptedMessage);
+console.log(decryptedMessage);
+
+function updateParagraph() {
+  const paragraph = document.getElementById("decryptedMessage");
+  paragraph.textContent = decryptedMessage;
+}
+
